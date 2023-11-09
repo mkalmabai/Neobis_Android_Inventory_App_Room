2a4f246ab59c5fccc8401ea405f83bd355bb9a0c
Margulan <akudajkul7@gmail.com>
2023-11-10 02:18:51 +0600

    created fragments, nav bar, nav component

--- app/src/main/java/com/example/neobis_android_inventory_app/MainActivity.kt
+++ app/src/main/java/com/example/neobis_android_inventory_app/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.neobis_android_inventory_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.neobis_android_inventory_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setCurrentFragment(MainFragment())
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.mainBar -> setCurrentFragment(MainFragment())
+                R.id.archiveBar -> setCurrentFragment(ArchiveFragment())
+            }
+            true
+        }
+
+    }
+    private fun setCurrentFragment(fragment:Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentBlank,fragment)
+        fragmentTransaction.commit()
     }
 }
